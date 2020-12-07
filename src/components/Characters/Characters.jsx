7aefd40d869806e98c1a54e3e89538b828020074
Maorguez <avila.mauro@gmail.com>
2020-12-07 02:26:54 -0500

    use/ useRef, useCallback, useMemo

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,22 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useReducer, useMemo, useRef, useCallback } from 'react';
+import Search from '../Search/Search';
+import useCharacters from '../../hooks/useCharacters';
 import styled from 'styled-components';
 
-const Characters = () => {
-  const [characters, setCharacters] = useState([]);
 
-  const getCharacters = async () => {
+const initialState = {
+  favorites: []
+}
+
+const API = 'https://rickandmortyapi.com/api/character/';
+
+//Reducer que se va encvargar de agregar a fovoritos
+const favoriteReducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+    default:
+      return state;
+  }
+};
+
+const Characters = () => {
+  // const [characters, setCharacters] = useState([]);
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+  const [search, setSearch] = useState('');
+  const searchInput = useRef(null);
+
+  const characters = useCharacters(API);
+  //Ahora se va utlizar custom hooks para la api
+  /* const getCharacters = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/character/');
     const data = await response.json();
     setCharacters(data.results);
   }
   useEffect(() => {
     getCharacters();
-  }, []);
+  }, []); */
   /* useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/')
       .then(response => response.json())
       .then(data => setCharacters(data.results));
   }, []); */
+
+  const handleClick = favorite => {
+    dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite });
+  }
+
+  /* const handleSearch = (event) => {
+    setSearch(event.target.value);
+  } */
+  //Busqueda con memoización
+  /* const handleSearch = () => {
+    setSearch(searchInput.current.value);
+  } */
+
+  //Busqueda con callback
+  const handleSearch = useCallback(() => {
+    setSearch(searchInput.current.value);
+  }, []);
 
   const Characters = styled.section`
     display: grid;
@@ -48,12 +92,36 @@ const Characters = () => {
     transition: 0.2s ease;
   `;
 
+  const Name = styled.h2`
+
+  `;
+  //Buesqueda simple
+  /* const filteredUsers = characters.filter((user) => {
+    return user.name.toLowerCase().includes(search.toLowerCase());
+  }) */
+
+  //Busqueda con useMemo
+  const filteredUsers = useMemo(() =>
+    characters.filter((user) => {
+      return user.name.toLowerCase().includes(search.toLowerCase());
+    }), [characters, search]
+  );
+
   return(
     <Characters>
-      {characters.map(character => (
+      {favorites.favorites.map(favorite => (
+        <li key={favorite.id}>
+          {favorite.name}
+        </li>
+      ))}
+      <Search search={search} searchInput={searchInput} handleSearch={handleSearch} />
+      {filteredUsers.map(character => (
         <Card key={character.id}>
           <Image src={character.image} alt="character" />
-          <h2>{character.name}</h2>
+          <Name>{character.name}</Name>
+          <button type="button" onClick={() => handleClick(character)}>
+            Agregar a Favoritos
+          </button>
         </Card>
       ))}
     </Characters>
